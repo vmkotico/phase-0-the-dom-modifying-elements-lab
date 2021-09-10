ff919a8e1301ceece284a78ff7cf68b7f10786ed
Vida May Kotico <vmkotico@yahoo.com>
2021-09-10 02:39:53 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+mainwithMainId = document.querySelector("main").remove()
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML("Vida is the champion");
